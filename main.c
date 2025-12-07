2bc6efe4979d3974997f5b9b7518c84c74d7422c
Rahul Hegde <rgh9883@rit.edu>
2025-12-07 13:56:09 -0500

    add string append and char insert funcs

--- main.c
+++ main.c
@@ -5,11 +5,20 @@
 
 int main() {
   String* str = string_create();
-  char_append(str, "Hello World");
 
-  printf("%s", str->data);
+  char_append(str, "Hello World");
+  printf("%s\n", str->data);
+  
+  String* str2 = string_create();
+  char_append(str2, "!");
+
+  // string_append(str, str2);
+  // printf("%s\n", str->data);
+  
+  char_insert(str, "NOT", 0);
+  printf("%s\n", str->data);
 
   string_free(str);
-
+  string_free(str2);
   return 1;
 }
--- string.c
+++ string.c
@@ -28,6 +28,41 @@ void char_append(String* str, char* add_char) {
   str->len = len + add_len;
 }
 
+void string_append(String* str, String* add_str) {
+  size_t add_len = add_str->len;
+
+  size_t len = str->len;
+  str->data = (char*) realloc(str->data, (len + add_len + 1) * sizeof(char));
+  for(int i = 0; i <= add_len; i++) {
+    str->data[len+i] = add_str->data[i];
+  }
+
+  str->len = len + add_len;
+}
+
+void char_insert(String* str, char* ins_char, size_t index) {
+  size_t ins_len = 0;
+  while(ins_char[ins_len] != '\0') {
+    ins_len++;
+  }
+
+  size_t len = str->len;
+  str->data = (char*) realloc(str->data, (len + ins_len + 1) * sizeof(char));
+
+  size_t i = len;
+  while(i >= index) {
+    str->data[i+ins_len] = str->data[i];
+    if(i == 0) break; // size_t unsigned so 0-1 -> inf
+    i--;
+  }
+  
+  for(int j = 0; j < ins_len; j++) {
+    str->data[j+index] = ins_char[j];
+  }
+
+  str->len = len + ins_len;
+}
+
 void string_free(String* str) {
   free(str->data);
   free(str);
